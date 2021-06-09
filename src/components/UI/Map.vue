2d4539b213b206fa83ba957f6370680922f32ddd
Owen <owen@howtank.com>
2021-06-09 23:02:39 +0200

    adding fetch method for map markers

--- src/components/UI/Map.vue
+++ src/components/UI/Map.vue
@@ -1,29 +1,48 @@
 <template>
-  <div class="w-2/3" id="mapContainer"></div>
+  <div class="w-2/3" id="mapContainer">
+  
+  </div>
 </template>
 
 <script>
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-
+import axios from 'axios'
 export default {
   name: "LeafletMap",
   data() {
     return {
         map: null,
+        posts: [],
     };
   },
   created() {
   },
+ methods:{
+    async fetchData () {
+        const res = await axios.get('http://localhost:6590/api/posts', {
+        })
+        this.posts = res.data
+        
+  },
+ },
   
   mounted() {
+    this.fetchData()
     this.$getLocation()
     .then(coordinates => {
     localStorage.setItem('coordinates', JSON.stringify(coordinates));
+
     this.map = L.map("mapContainer").setView([coordinates.lat,coordinates.lng], 200);
     L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
     }).addTo(this.map);
-   this.currentMarker = L.circle([coordinates.lat,coordinates.lng],{radius: 5}).addTo(this.map);
+
+  this.currentMarker = L.circle([coordinates.lat,coordinates.lng],{radius: 5}).addTo(this.map);
+  /*this.posts.forEach(function(post){
+    console.log(post)
+   this.marker.id =  L.icon([post.gpsPositionLat,post.gpsPositionLong],{radius: 5}).addTo(this.map);
+    
+  }); */
 
     setInterval(function() {
       localStorage.setItem('coordinates', JSON.stringify(coordinates));
@@ -31,5 +50,6 @@ export default {
     }, 60 * 1000);
     
   }
+  
 };
 </script>
